cfca2277ced62fbd4cc27a9da2756c80f746c089
sebastian orellana <sebastian.orellana.v@usach.cl>
2019-09-27 00:58:54 -0300

    parte2a and parte2c fixed

--- parte2a.asm
+++ parte2a.asm
@@ -4,90 +4,164 @@
 ############### A) Calcular la multiplicación de dos enteros mediante la implemenatcion de subrutinas
 
 #Segmento de datos
-.data	################   ENTEROS QUE SE VAN A MULTIPLICAR   ##############################
-	entero1: .word 3	#Entero 1
-	entero2: .word -5	#Entero 2
+.data	
+	mensaje1: .asciiz "Por favor ingrese el primer entero: "
+	mensaje2: .asciiz "Por favor ingrese el segundo entero: "
 	mensaje: .asciiz "El resultado es: "
 	
 #Segmento de Texto
 .text
-	#Main Process
-	main:	
-		#Cargar numeros que se desean multiplicar
-		lw $a0, entero1	#cargar el entero en $a0 para pasarlo como argumento a las sub-rutinas
-		lw $a1, entero2	#cargar el entero en $a1 para pasarlo como argumento a las sub-rutinas
-		
-		# $a2 = contador = 0
-		li $a2, 0
-		
-		#si $t0 > 0
-		bgtz $a0, multiplicacion1	#ir a la sub-rutina multiplicacion1
-		b else				#ir a la sub-rutina else
-		
-	else:	#si $a1 > 0
-		bgtz $a1, multiplicacion2	#ir a la subrutina multiplicacion2
-		#sino 
-		jal convertirAPositivo	#convertir los numeros a positivos
-		move $a3, $v1			#guardar el numero convertido en $a3
-		move $a0, $a1			#preparar el registro $a0 para convertir el otro numero
-		li $v1, 0			#restaurar el valor del registro $v1
-		jal convertirAPositivo	#convertir el otro numero
-		move $a0, $v1			#guardar el segundo numero convertido
-		move $a1, $a3			#rescuperar el numero anteriormente convertido
-		li $a3, 0			#restaurar el valor del registro $a3
-		li $v1, 0			#restaurar el valor del registro $v1
-		b multiplicacion1		#multiplicar los numeros convertidos
+#Main Process
+main:	
+	#Mostrar mensaje 1 al usuario: "Por favor ingrese el primer entero: "
+	li $v0, 4	    	#indicar al sistema que se quiere mostrar un string por pantalla
+	la $a0, mensaje1     #cargar la dirección de memoria del mensaje 1
+	syscall		#llamada al sistema: mostrar el mensaje por pantalla
 	
-	#se utiliza la siguiente multiplicacion cuando ambos numeros son positivos o cuando
-	#el primer numero ingresado es positivo y el otro negativo
-	# Argumentos: $a0 = numero1
-	#		$a1 = numero2
-	#		$a2 = contador = 0
-	# Resultado:  $v1 
-	multiplicacion1:	
-			  add $v1, $v1, $a1			#sumar el numero2 al registro donde se va guardando el resultado
-		      	  addi $a2, $a2, 1			#Sumar 1 al contador 
-			  bne $a2, $a0, multiplicacion1	# mientras contador < numero1 volver a la etiqueta multiplicacion
-			  li $a2, 0				#restaurar valor de $a2
-		  	  b exit				#ir a la etiqueta exit
+	#Pedir Entero al usuario
+	jal pedirEntero	#ir al procedimiento pedirEntero
 	
-	#se utiliza la siguiente multiplicacion cuando el primer numero ingresado es 
-	#negativo y el otro es positivo
-	# Argumentos: $a0 = numero1
-	#		$a1 = numero2
-	#		$a2 = contador = 0
-	# Resultado:  $v1 	
-	multiplicacion2:
-			  add $v1, $v1, $a0			#sumar el numero1 al registro donde se va guardando el resultado
-		      	  addi $a2, $a2, 1			#Sumar 1 al contador 
-			  bne $a2, $a1, multiplicacion2	# mientras contador < numero1 volver a la etiqueta multiplicacion
-			  li $a2, 0				#restaurar valor de $a2
-		  	  b exit				#ir a la etiqueta exit 
+	#Guardar el dato ingresado en el registro $a1
+	move $a1, $v0 	    #mover el valor de $v0 a $a1
+			    	    # $a1 = argumento 1 de la subrutina encontrarMenor
 	
-	#se utiliza la siguiente sub-rutina para convertir un numero negativo en positivo sin multiplicar 
-	#por menos 1, sino utilizando solo sumas
-	convertirAPositivo:	
-			  sub $v1, $zero, $a0		#se convierte el registro $a0 a positivo
-			  jr $ra  		  		#volver a la multiplicacion
-			  
-	#rutina para imprimir resultado y terminar el programa
-	exit:	
-		#Mostrar mensaje al usuario
-		li $v0, 4	     #indicar al sistema que se quiere mostrar un string por pantalla
-		la $a0, mensaje    #cargar la dirección de memoria del mensaje 
-		syscall     	     #llamada al sistema: mostrar el mensaje por pantalla
+	#Mostrar mensaje 2 al usuario: "Por favor ingrese el segundo entero: "
+	li $v0, 4	    #indicar al sistema que se quiere mostrar un string por pantalla
+	la $a0, mensaje2    #cargar la dirección de memoria del mensaje 2
+	syscall		    #llamada al sistema: mostrar el mensaje por pantalla
+	
+	#Pedir Entero al usuario
+	jal pedirEntero	#ir al procedimiento pedirEnter
+	
+	#Guardar el dato ingresado en el registro $a2
+	move $a2, $v0 	    #mover el valor de $v0 a $a2
+			    	    # $a2 = argumento 2 de la subrutina encontrarMenor
+	
+	#ir a calcularMultiplicacion
+	jal calcularMultiplicacion
+	
+	#Mostrar mensaje 2 al usuario: "El resultado es: "
+	li $v0, 4	    #indicar al sistema que se quiere mostrar un string por pantalla
+	la $a0, mensaje    #cargar la dirección de memoria del mensaje 2
+	syscall		    #llamada al sistema: mostrar el mensaje por pantalla
+	
+	#imprimir por pantalla el resultado de la subrutina (guardado en $v1)	    
+	move $a0, $v1		# preprar numero para imprimir
+	li $v0, 1	       #indicarle al sistema que se quiere mostrar un entero
+	syscall              #llamada al sistema: mostrar el entero por pantalla
+	
+	#terminar Programa
+	j terminarPrograma
+
+#Procedimiento que calcula la multiplicacion de dos numeros
+#Argumentos:  $a1 = numero 1
+#		$a2 = numero 2
+#Resultado:   $v1
+calcularMultiplicacion:
+		beqz $a1, esCero	#si uno de los numeros ingresados es 0
+		beqz $a2, esCero
 		
-		#imprimir por pantalla el resultado de la subrutina (guardado en $v1)	    
-		li $v0, 1	    #indicarle al sistema que se quiere mostrar un entero
-		move $a0, $v1       #mover el entero de $v1 a $a0
-		syscall             #llamada al sistema: mostrar el entero por pantalla
+		addi $sp, $sp, -28	# Modificar el stackPointer
+		sw $ra, 24($sp)	# guardar el valor de los registros que van a ser utilizados
+		sw $t0, 20($sp)	
+		sw $t1, 16($sp)
+		sw $t2, 12($sp)
+		sw $t3, 8($sp)
+		sw $t4, 4($sp)
+		sw $t5, 0($sp)
 		
-		#Terminar el programa
-		li $t0, 0	    #restaurar el valor del registro $t0
-		li $t1, 0	    #restaurar el valor del registro $t1
-		li $v0, 10	    #indicarle al sistema que se quiere finalizar el programa	    
+		# preparar los registros que se van a utilizar
+		move $t0, $a1	  # $t0 = numero 1
+		move $t1, $a2	  # $t1 = numero 2
+		li $t2, 0	  # $t2 = signo numero 1
+		li $t3, 0	  # $t3 = signo numero 2
+		li $t4, 0	  # $t4 = signo resultado
+		li $t5, 0	  # $t4 = resultado
+		
+		#verificar signos de los nuemros
+		bltz $t0, N1negativo		# if numero 1 < 0 ir a N1Negativo
+		bltz $t1, resultNegativ1	# elseif numero 2 < 0 ir a resultNegativ
+		b multiplicar			# else ir a multiplicar
+		
+		N1negativo: 	
+			   	li $t2, 1			# cambiar signo 1
+				move $a0, $t0			# mover numero 1 al registro $a0
+			  	jal cambiarSigno		# ir a cambiarSigno
+				move $t0, $v0			# mover $v0 a $t0
+				   
+				bltz $t1, resultPositiv	# if numero 2 < 0 ir a resultPositiv
+				b resultNegativ2		# else ir a 
+			
+		resultPositiv:			
+				li $t3, 1			# cambiar signo 2
+				move $a0, $t1			# mover numero 2 al registro $a0
+				jal cambiarSigno		# ir a cambiarSigno
+				move $t1, $v0			# mover $v0 a $t1
+				b multiplicar			# ir a multiplicar
+			
+		resultNegativ1:
+				li $t3, 1			# cambiar signo 2
+				move $a0, $t1			# mover numero 2 al registro $a0
+				jal cambiarSigno		# ir a cambiarSigno
+				move $t1, $v0			# mover $v0 a $t1
+				li $t4, 1			# cambiar signo del resultado
+				b multiplicar			# ir a multiplicar
+			
+		resultNegativ2:
+				li $t4, 1			# cambiar signo del resultado
+				b multiplicar			# ir a multiplicar
+			
+			#Ciclo para multiplicar
+			multiplicar:
+					add $t5, $t5, $t0		# sumar el numero 1 al registro donde se va guardando el resultado
+		      	  		addi $t1, $t1, -1		# restar 1 al numero 2
+			  		bgtz $t1, multiplicar     	# mientras numero 2 > 0 volver a la etiqueta multiplicar
+		  	  		move $v1, $t5			# mover $t5 a $v1 (resultado)
+		  	  		
+		  	  		beq $t4, 1, verificarSigno	# si $t4 == 1 (es negativo) entonces se debe cambiar el signo
+		  	  		b restaurarRegistros		#ir a restaurarRegistros
+		  	 
+		  	 verificarSigno:
+		  	 		   move $a0, $t5		# mover $t5 a $a0 (argumento)
+		  	 		   jal cambiarSigno		# ir a cambiarSigno
+		  	 		   move $v1, $v0		# mover $v0 a $v1 (resultado)
+		  	 		   b restaurarRegistros	# ir a restaurarRegistros
+		  	 
+		  	 restaurarRegistros: 
+						lw $t5, 0($sp)	#Restaurar registros utilizados
+						lw $t4, 4($sp)
+						lw $t3, 8($sp)
+						lw $t2, 12($sp)
+						lw $t1, 16($sp)
+						lw $t0, 20($sp)
+						lw $ra, 24($sp)
+		  	 			addi $sp, $sp, 28	#restaurar stackPointer
+		  	 			
+		  	 			jr $ra			# Salir del procedimiento
+		  	 
+		  	 #Si uno de los numeros ingresados es 0
+		  	 esCero: 
+		  	 	li $v1, 0
+		  	 	jr $ra
+		  	 	
+#Procedimiento para cambiar de signo
+#Argumentos:  $a0 = numero
+#Resultado:   $v0
+cambiarSigno:
+		sub $v0, $zero, $a0
+		jr $ra
+
+#Pedir un entero al usuario
+pedirEntero:
+		li $v0, 5            #indicar al sistema que se quiere hacer un input de un entero
+		syscall	       #llamada al sistema: pedir dato al usuario
+		jr $ra		       #vovler hasta donde se llamo al procedimiento
+
+#Terminar el programa
+terminarPrograma:
+		li $v0, 10	    	    #indicarle al sistema que se quiere finalizar el programa	    
 		syscall		    #llamada al sistema: finalizar el programa
-	
+		
 	###### EL RESULTADO DE LA MULTIPLICACION ESTA EN EL REGISTRO $v1 ####################
 	###### EL RESULTADO DE LA MULTIPLICACION ESTA EN EL REGISTRO $v1 ####################
 	###### EL RESULTADO DE LA MULTIPLICACION ESTA EN EL REGISTRO $v1 ####################
--- parte2c.asm
+++ parte2c.asm
@@ -9,84 +9,219 @@
 	mensaje2: .asciiz "Ingrese el divisor: "
 	mensaje3: .asciiz "El resultado es: "
 	mensaje4: .asciiz "Bienvenid@, este programa divide dos numeros\n"
+	mensaje5: .asciiz "No se puede dividir por 0"
+	uno:      .float 1.0
+	decimal:  .float 0.01
+	cero:     .float 0.0
 
 #Segmento de Texto
 .text
-	main:
-		#Mostrar bienvenida al usuario
-		li $v0, 4	    #indicar al sistema que se quiere mostrar un string por pantalla
-		la $a0, mensaje4    #cargar la dirección de memoria del mensaje 1
-		syscall		    #llamada al sistema: mostrar el mensaje por pantalla
-		
-		#Mostrar mensaje 1 al usuario
-		li $v0, 4	    #indicar al sistema que se quiere mostrar un string por pantalla
-		la $a0, mensaje1    #cargar la dirección de memoria del mensaje 1
-		syscall		    #llamada al sistema: mostrar el mensaje por pantalla
+#Main Process
+main:	
+	#Mostrar bienvenida al usuario
+	li $v0, 4	    #indicar al sistema que se quiere mostrar un string por pantalla
+	la $a0, mensaje4    #cargar la dirección de memoria del mensaje 1
+	syscall		    #llamada al sistema: mostrar el mensaje por pantalla
 	
-		#Pedir un numero al usuario
-		li $v0, 5           #indicar al sistema que se quiere hacer un input de un entero
-		syscall		    #llamada al sistema: pedir dato al usuario
+	#Mostrar mensaje 1 al usuario: "Por favor ingrese el primer entero: "
+	li $v0, 4	    	#indicar al sistema que se quiere mostrar un string por pantalla
+	la $a0, mensaje1     #cargar la dirección de memoria del mensaje 1
+	syscall		#llamada al sistema: mostrar el mensaje por pantalla
 	
-		#Guardar el dato ingresado en el registro $a1
-		move $a1, $v0 	    #mover el valor de $v0 a $a1
+	#Pedir Entero al usuario
+	jal pedirEntero	#ir al procedimiento pedirEntero
+	
+	#Guardar el dato ingresado en el registro $a1
+	move $a1, $v0 	    #mover el valor de $v0 a $a1
 			    	    # $a1 = argumento 1 de la subrutina encontrarMenor
 	
-		#Mostrar mensaje 2 al usuario
-		li $v0, 4	    #indicar al sistema que se quiere mostrar un string por pantalla
-		la $a0, mensaje2    #cargar la dirección de memoria del mensaje 2
-		syscall		    #llamada al sistema: mostrar el mensaje por pantalla
+	#Mostrar mensaje 2 al usuario: "Por favor ingrese el segundo entero: "
+	li $v0, 4	    #indicar al sistema que se quiere mostrar un string por pantalla
+	la $a0, mensaje2    #cargar la dirección de memoria del mensaje 2
+	syscall		    #llamada al sistema: mostrar el mensaje por pantalla
 	
-		#Pedir un numero al usuario
-		li $v0, 5           #indicar al sistema que se quiere hacer un input de un entero
-		syscall		    #llamada al sistema: pedir dato al usuario
-		
-		#Guardar el dato ingresado en el registro $a2
-		move $a2, $v0 	    #mover el valor de $v0 a $a2
-					    # $a1 = argumento 1 de la subrutina encontrarMenor
-		
-		#Preparar contador (argumento) para el procedimiento division
-		li $a3, 0
-		
-		#Ir al procedimiento division
-		jal divisionEntera
-		
-		#Guardar elm resultado de la division en un registro temporal
-		move $t0, $v1
-		
-		#si el dividendo no es 0 (se debe seguir dividiendo) 
-		bne $a1, $zero, multX100		#saltar a la etiqueta multX100
-		
-	#Procedimiento para dividir enteros
-	#Argumentos:  $a1 = Dividendo
-	#		$a2 = Divisor
-	#		$a3 = contador(resultado) = 0
-	#Resultado:  	$v1	
-	divisionEntera:
-				sub $a1, $a1, $a2		# Dividendo = Dividendo - Divisor
-				addi $a3, $a3, 1		# Sumar 1 al contador
-				bge $a1, $a2, divisionEtera	# Continuar restando mientras el dividendo sea mayor que el divisor 
-				move $v1, $a3			# mover el resultado al registro de retornos de resultados
-				j $ra				#volver al main
-				
-	#Procedimiento utilizado para multiplicar el dividendo por 10 en caso de tener que realizar una division decimal
-	#Argumentos:  $a0 = contador = 0
-	#		$a1 = dividendo
-	#Resultado: 	$v1
-	multX100: 
-		  addi $sp, $sp, -4
-		  sw $a0, 0($sp)
-		  li $a0, 0
-		  whileMult:
-			 	add $v1, $v1, $a1			#sumar el numero2 al registro donde se va guardando el resultado
-		      	 	addi $a0, $a0, 1			#Sumar 1 al contador 
-			 	bne $a0, 10, multX100		# mientras contador < numero1 volver a la etiqueta multiplicacion
-		  
-		  #Restaurar Valor de $a0
-		  lw
-		  li $a0, 0				#restaurar valor de $a2
-		  b divisionDecimal			#ir a la etiqueta divisionDecimal
+	#Pedir Entero al usuario
+	jal pedirEntero	#ir al procedimiento pedirEnter
 	
-	divisionDecimal: 	
-				sub $a1, $a1, $a2
-				
-	exit:
+	#Guardar el dato ingresado en el registro $a2
+	move $a2, $v0 	    #mover el valor de $v0 a $a2
+			    	    # $a2 = argumento 2 de la subrutina encontrarMenor
+	
+	#ir a calcularMultiplicacion
+	jal calcularDivision
+	
+	#Mostrar mensaje 2 al usuario: "El resultado es: "
+	li $v0, 4	    #indicar al sistema que se quiere mostrar un string por pantalla
+	la $a0, mensaje3    #cargar la dirección de memoria del mensaje 2
+	syscall		    #llamada al sistema: mostrar el mensaje por pantalla
+	
+	#imprimir por pantalla el resultado de la division	    
+	lwc1 $f1, 0($v1)
+	li $v0, 2	       #indicarle al sistema que se quiere mostrar un entero
+	mfc1 $a0, $f1        #mover el entero de $v1 a $a0
+	syscall              #llamada al sistema: mostrar el entero por pantalla
+	
+	#terminar Programa
+	j terminarPrograma
+	
+calcularDivision:	
+		beqz $a1, divisionCero	#si el primer numero es 0 la division es 0
+		beqz $a2, divisionInvalida	#si el segundo numero es 0 la division es invalida
+		
+		addi $sp, $sp, -32	# Modificar el stackPointer
+		sw $ra, 28($sp)	# guardar el valor de los registros que van a ser utilizados
+		sw $t0, 24($sp)	
+		sw $t1, 20($sp)
+		sw $t2, 16($sp)
+		sw $t3, 12($sp)
+		sw $t4, 8($sp)
+		sw $t5, 4($sp)
+		sw $t6, 0($sp)
+		
+		# preparar los registros que se van a utilizar
+		move $t0, $a1	  # $t0 = numero 1
+		move $t1, $a2	  # $t1 = numero 2
+		li $t2, 0	  # $t2 = signo numero 1
+		li $t3, 0	  # $t3 = signo numero 2
+		li $t4, 0	  # $t4 = signo resultado
+		li $t5, 0	  # $t5 = resultado Entero
+		li $t6, 0	  # $t6 = resultado Decimal
+		l.s $f0, uno	
+		l.s $f1, decimal
+		l.s $f2, cero
+		
+		#verificar signos de los nuemros
+		bltz $t0, N1negativoDiv		# if numero 1 < 0 ir a N1Negativo
+		bltz $t1, resultNegativ1Div		# elseif numero 2 < 0 ir a resultNegativ
+		blt $t0, $t1, dividirDecimales
+		b dividirEnteros			# else ir a dividirEnteros
+		
+		N1negativoDiv: 	
+			   	li $t2, 1			# cambiar signo 1
+				move $a0, $t0			# mover numero 1 al registro $a0
+			  	jal cambiarSigno		# ir a cambiarSigno
+				move $t0, $v0			# mover $v0 a $t0
+				   
+				bltz $t1, resultPositivDiv	# if numero 2 < 0 ir a resultPositiv
+				b resultNegativ2Div		# else ir a 
+			
+		resultPositivDiv:			
+				li $t3, 1			# cambiar signo 2
+				move $a0, $t1			# mover numero 2 al registro $a0
+				jal cambiarSigno		# ir a cambiarSigno
+				move $t1, $v0			# mover $v0 a $t1
+				blt $t0, $t1, dividirDecimales
+				b dividirEnteros		# ir a dividirEnteros
+			
+		resultNegativ1Div:
+				li $t3, 1			# cambiar signo 2
+				move $a0, $t1			# mover numero 2 al registro $a0
+				jal cambiarSigno		# ir a cambiarSigno
+				move $t1, $v0			# mover $v0 a $t1
+				li $t4, 1			# cambiar signo del resultado
+				blt $t0, $t1, dividirDecimales
+				b dividirEnteros		# ir a dividirEnteros
+			
+		resultNegativ2Div:
+				li $t4, 1			# cambiar signo del resultado
+				blt $t0, $t1, dividirDecimales
+				b dividirEnteros		# ir a dividirEnteros
+			
+			#Ciclo para dividir
+			dividirEnteros:
+					sub $t0, $t0, $t1			# Dividendo = Dividendo - Divisor
+		      	  		add.s $f2, $f2, $f0			# sumar 1 al resultado
+			  		bge $t0, $t1, dividirEnteros	# mientras numero 2 > 0 seguir dividiendo
+		  	  		
+		  	  		addi $sp, $sp, -4		# modificar stackPointer
+					sw $ra, 0($sp)		# guardar el valor del registro $ra
+					jal dividirDecimales 	# dividir parte decimal
+					lw $ra, 0($sp)		# restaurar registro $ra
+					addi $sp, $sp, 4		# restaurar stackPointer
+		  	  		
+		  	  		beq $t4, 1, verificarSignoDiv	# si $t4 == 1 (es negativo) entonces se debe cambiar el signo
+		  	  		b restaurarRegistrosDiv		# ir a restaurarRegistros
+		  	 
+		  	 verificarSignoDiv:
+		  	 		   mfc1 $a0, $f2		# mover $t5 a $a0 (argumento)
+		  	 		   addi $sp, $sp, -4		# modificar stackPointer
+					   sw $ra, 0($sp)		# guardar $ra en el stack
+		  	 		   jal cambiarSignoFloat	# ir a cambiarSigno
+		  	 		   lw $ra, 0($sp)		# restaurar registro $ra
+					   addi $sp, $sp, 4		# restaurar stackPointer
+		  	 		   move $v1, $v0		# mover $v0 a $v1 (resultado)	 		   
+		  	 		   b restaurarRegistrosDiv	# ir a restaurarRegistros
+		  	 
+		  	 restaurarRegistrosDiv: 
+						lw $t6, 0($sp)	# Restaurar registros utilizados
+						lw $t5, 4($sp)
+						lw $t4, 8($sp)
+						lw $t3, 12($sp)
+						lw $t2, 16($sp)
+						lw $t1, 20($sp)
+						lw $t0, 24($sp)
+						lw $ra, 28($sp)
+		  	 			addi $sp, $sp, 32	# restaurar stackPointer
+		  	 			
+		  	 			jr $ra			# Salir del procedimiento
+		  	 
+		  	 dividirDecimales:
+		  			bnez $t0, multX100
+		  			jr $ra
+		  			multX100:
+			 			addi $t6, $t6, 100	#sumar 100 al registro donde se va guardando el resultado
+		      	 			addi $t0, $t0, -1	#Sumar 1 al contador 
+						bgtz $t0, multX100	# mientras $t0 > 0 volver a multX100
+		  				move $t0, $t6
+		  				li $t6, 0
+					
+					divDecimales:
+							sub $t0, $t0, $t1		# Dividendo = Dividendo - Divisor
+		      	  				add.s $f2, $f2, $f1		# sumar 1 al resultado (parte decimal)
+			  				bge $t0, $t1, divDecimales	# mientras numero 1 > numero 2 seguir dividiendo
+		  	  				mfc1 $v0, $f2 		# mover $t5 a $v1 (resultado)		  			
+							jr $ra	
+		  	   
+		  	divisionCero:
+		  			l.s $f0, cero
+		  			mfc1 $v0, $f0
+		  			jr $ra
+		  			
+		  	divisionInvalida:
+					#Mostrar mensaje 2 al usuario
+			  		li $v0, 4	        #indicar al sistema que se quiere mostrar un string por pantalla
+			  		la $a0, mensaje5    #cargar la dirección de memoria del mensaje 2
+			  		syscall     	 #llamada al sistema: mostrar el mensaje por pantalla
+			  
+			  		j terminarPrograma
+	
+#Procedimiento para cambiar de signo
+#Argumentos:  $a0 = numero
+#Resultado:   $v0
+cambiarSigno:
+		sub $v0, $zero, $a0
+		jr $ra
+
+#Procedimiento para cambiar de signo a uun numero flotante
+#Argumentos:  $a0 = numero
+#Resultado:   $v0
+cambiarSignoFloat:
+		sub $v0, $zero, $a0
+		jr $ra
+
+#Pedir un entero al usuario
+pedirEntero:
+		li $v0, 5            #indicar al sistema que se quiere hacer un input de un entero
+		syscall	       #llamada al sistema: pedir dato al usuario
+		jr $ra		       #vovler hasta donde se llamo al procedimiento
+
+#Terminar el programa
+terminarPrograma:
+		li $v0, 10	    	    #indicarle al sistema que se quiere finalizar el programa	    
+		syscall		    #llamada al sistema: finalizar el programa
+		
+	###### EL RESULTADO DE LA MULTIPLICACION ESTA EN EL REGISTRO $v1 ####################
+	###### EL RESULTADO DE LA MULTIPLICACION ESTA EN EL REGISTRO $v1 ####################
+	###### EL RESULTADO DE LA MULTIPLICACION ESTA EN EL REGISTRO $v1 ####################
+	###### EL RESULTADO DE LA MULTIPLICACION ESTA EN EL REGISTRO $v1 ####################	
+	
