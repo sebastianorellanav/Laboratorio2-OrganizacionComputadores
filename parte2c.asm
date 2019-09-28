d72ffeba886cffffc272eb8d319aa4e687bd3b08
sebastian orellana <sebastian.orellana.v@usach.cl>
2019-09-28 01:40:03 -0300

    part2c working

--- parte2c.asm
+++ parte2c.asm
@@ -5,14 +5,15 @@
 
 #Segmento de Datos
 .data 
-	mensaje1: .asciiz "Ingrese el dividendo: "
-	mensaje2: .asciiz "Ingrese el divisor: "
-	mensaje3: .asciiz "El resultado es: "
-	mensaje4: .asciiz "Bienvenid@, este programa divide dos numeros\n"
-	mensaje5: .asciiz "No se puede dividir por 0"
-	uno:      .float 1.0
-	decimal:  .float 0.01
-	cero:     .float 0.0
+	mensaje1: 	   .asciiz "Ingrese el dividendo: "
+	mensaje2: 	   .asciiz "Ingrese el divisor: "
+	mensaje3: 	   .asciiz "El resultado es: "
+	mensaje4: 	   .asciiz "Bienvenid@, este programa divide dos numeros\n"
+	mensaje5: 	   .asciiz "No se puede dividir por 0"
+	parteEntera:	   .double 1.00
+	parteDecimal:    .double 0.10
+	parteCentecimal: .double 0.01
+	ceroDouble:      .double 0.00
 
 #Segmento de Texto
 .text
@@ -56,156 +57,174 @@ main:
 	syscall		    #llamada al sistema: mostrar el mensaje por pantalla
 	
 	#imprimir por pantalla el resultado de la division	    
-	lwc1 $f1, 0($v1)
-	li $v0, 2	       #indicarle al sistema que se quiere mostrar un entero
-	mfc1 $a0, $f1        #mover el entero de $v1 a $a0
+	li $v0, 3	       #indicarle al sistema que se quiere mostrar un entero
+	mov.d $f12, $f4       #mover el entero de $f4 a $f12
 	syscall              #llamada al sistema: mostrar el entero por pantalla
 	
 	#terminar Programa
 	j terminarPrograma
 	
 calcularDivision:	
-		beqz $a1, divisionCero	#si el primer numero es 0 la division es 0
-		beqz $a2, divisionInvalida	#si el segundo numero es 0 la division es invalida
+	beqz $a1, divisionCero	#si el primer numero es 0 la division es 0
+	beqz $a2, divisionInvalida	#si el segundo numero es 0 la division es invalida
+	
+	#GUARDAR EN EL STACK LOS REGISTROS QUE SE VAN A UTILIZAR	
+	addi $sp, $sp, -44	# Modificar el stackPointer
+	sw $ra, 40($sp)	# guardar el valor de los registros que van a ser utilizados
+	sw $t0, 36($sp)	
+	sw $t1, 32($sp)
+	sw $t2, 28($sp)
+	sw $t3, 24($sp)
+	sw $t4, 20($sp)
+	sw $t5, 16($sp)
+	sdc1 $f2, 8($sp)
+	sdc1 $f6, 0($sp)
+	
+	# preparar los registros que se van a utilizar
+	move $t0, $a1	  # $t0 = numero 1
+	move $t1, $a2	  # $t1 = numero 2
+	li $t2, 0	  # $t2 = signo numero 1
+	li $t3, 0	  # $t3 = signo numero 2
+	li $t4, 0	  # $t4 = signo resultado
+	li $t5, 0	  # $t5 = auxiliar
+	
+	#verificar signos de los nuemros
+	bltz $t0, N1negativoDiv		# if numero 1 < 0 ir a N1Negativo
+	bltz $t1, resultadoNegativo1Div		# elseif numero 2 < 0 ir a resultNegativ
+	b dividirParteEntera			# ir a dividirEnteros
 		
-		addi $sp, $sp, -32	# Modificar el stackPointer
-		sw $ra, 28($sp)	# guardar el valor de los registros que van a ser utilizados
-		sw $t0, 24($sp)	
-		sw $t1, 20($sp)
-		sw $t2, 16($sp)
-		sw $t3, 12($sp)
-		sw $t4, 8($sp)
-		sw $t5, 4($sp)
-		sw $t6, 0($sp)
-		
-		# preparar los registros que se van a utilizar
-		move $t0, $a1	  # $t0 = numero 1
-		move $t1, $a2	  # $t1 = numero 2
-		li $t2, 0	  # $t2 = signo numero 1
-		li $t3, 0	  # $t3 = signo numero 2
-		li $t4, 0	  # $t4 = signo resultado
-		li $t5, 0	  # $t5 = resultado Entero
-		li $t6, 0	  # $t6 = resultado Decimal
-		l.s $f0, uno	
-		l.s $f1, decimal
-		l.s $f2, cero
-		
-		#verificar signos de los nuemros
-		bltz $t0, N1negativoDiv		# if numero 1 < 0 ir a N1Negativo
-		bltz $t1, resultNegativ1Div		# elseif numero 2 < 0 ir a resultNegativ
-		blt $t0, $t1, dividirDecimales
-		b dividirEnteros			# else ir a dividirEnteros
-		
-		N1negativoDiv: 	
-			   	li $t2, 1			# cambiar signo 1
-				move $a0, $t0			# mover numero 1 al registro $a0
-			  	jal cambiarSigno		# ir a cambiarSigno
-				move $t0, $v0			# mover $v0 a $t0
-				   
-				bltz $t1, resultPositivDiv	# if numero 2 < 0 ir a resultPositiv
-				b resultNegativ2Div		# else ir a 
+	N1negativoDiv: 	
+		li $t2, 1			# cambiar signo 1
+		move $a0, $t0			# mover numero 1 al registro $a0
+		jal cambiarSigno		# ir a cambiarSigno
+		move $t0, $v0			# mover $v0 a $t0   
+		bltz $t1, resultadoPositivoDiv	# if numero 2 < 0 ir a resultPositiv
+		b resultadoNegativo2Div		# else ir a 
 			
-		resultPositivDiv:			
+		resultadoPositivoDiv:			
 				li $t3, 1			# cambiar signo 2
 				move $a0, $t1			# mover numero 2 al registro $a0
 				jal cambiarSigno		# ir a cambiarSigno
 				move $t1, $v0			# mover $v0 a $t1
-				blt $t0, $t1, dividirDecimales
-				b dividirEnteros		# ir a dividirEnteros
+				b dividirParteEntera		# ir a dividirEnteros
 			
-		resultNegativ1Div:
+		resultadoNegativo1Div:
 				li $t3, 1			# cambiar signo 2
 				move $a0, $t1			# mover numero 2 al registro $a0
 				jal cambiarSigno		# ir a cambiarSigno
 				move $t1, $v0			# mover $v0 a $t1
 				li $t4, 1			# cambiar signo del resultado
-				blt $t0, $t1, dividirDecimales
-				b dividirEnteros		# ir a dividirEnteros
+				b dividirParteEntera		# ir a dividirEnteros
 			
-		resultNegativ2Div:
+		resultadoNegativo2Div:
 				li $t4, 1			# cambiar signo del resultado
-				blt $t0, $t1, dividirDecimales
-				b dividirEnteros		# ir a dividirEnteros
-			
-			#Ciclo para dividir
-			dividirEnteros:
-					sub $t0, $t0, $t1			# Dividendo = Dividendo - Divisor
-		      	  		add.s $f2, $f2, $f0			# sumar 1 al resultado
-			  		bge $t0, $t1, dividirEnteros	# mientras numero 2 > 0 seguir dividiendo
-		  	  		
-		  	  		addi $sp, $sp, -4		# modificar stackPointer
-					sw $ra, 0($sp)		# guardar el valor del registro $ra
-					jal dividirDecimales 	# dividir parte decimal
-					lw $ra, 0($sp)		# restaurar registro $ra
-					addi $sp, $sp, 4		# restaurar stackPointer
-		  	  		
-		  	  		beq $t4, 1, verificarSignoDiv	# si $t4 == 1 (es negativo) entonces se debe cambiar el signo
-		  	  		b restaurarRegistrosDiv		# ir a restaurarRegistros
+				b dividirParteEntera		# ir a dividirEnteros
+		
+	dividirParteEntera:
+		# $f4 = resultado
+		l.d $f4, ceroDouble			# cargar en $f4 un 0.0
+		l.d $f6, ceroDouble
+		blt $t0, $t1, dividirParteDecimal	# si no hay parte entera para dividir, dividir parteDecimal
+		l.d $f2, parteEntera			# cargar en $f2 un 1.0
+				
+		# Ir al ciclo para dividir
+		jal cicloDividir		# dividir parte decimal
+		
+		add.d $f4, $f4, $f6
+						
+		# si dividendo = 0 ir a terminar
+		beqz $t0, terminarDivision
+		
+	dividirParteDecimal:
+		#Multiplicar dividendo x 10
+		jal multX10
+		l.d $f6, ceroDouble		
+		blt $t0, $t1, dividirParteCentecimal	#si no se puede ir a dividir la parteCentecimal				
+		l.d $f2, parteDecimal			#cargar en $f2 un 0.1
+				
+		# Ir al ciclo para dividir
+		jal cicloDividir		# dividir parte decimal	
+		
+		add.d $f4, $f4, $f6
+						
+		# si dividendo = 0 ir a terminar
+		beqz $t0, terminarDivision
+		
+	dividirParteCentecimal:
+		#Multiplicar x 10
+		li $t5, 0
+		jal multX10
+		l.d $f6, ceroDouble		
+		blt $t0, $t1, terminarDivision	#si no se puede dividir, terminar el procedimiento
+		l.d $f2, parteCentecimal	#cargar en $f2 un 0.01
+				
+		# Ir al ciclo para dividir
+		jal cicloDividir		# dividir parte decimal
+		
+		add.d $f4, $f4, $f6
+						
+		b terminarDivision		
+				
+	
+	#Ciclo para dividir
+	cicloDividir:
+		sub $t0, $t0, $t1			# Dividendo = Dividendo - Divisor
+		add.d $f6, $f6, $f2			# sumar 1 al resultado
+		bge $t0, $t1, cicloDividir	# mientras dividendo >= divisor seguir dividiendo
+		jr $ra
+	
+	#Terminar el procedimiento	  	  		
+	terminarDivision:
+		jal verificarSignoResultado	 # verificar el signo del resultado
 		  	 
-		  	 verificarSignoDiv:
-		  	 		   mfc1 $a0, $f2		# mover $t5 a $a0 (argumento)
-		  	 		   addi $sp, $sp, -4		# modificar stackPointer
-					   sw $ra, 0($sp)		# guardar $ra en el stack
-		  	 		   jal cambiarSignoFloat	# ir a cambiarSigno
-		  	 		   lw $ra, 0($sp)		# restaurar registro $ra
-					   addi $sp, $sp, 4		# restaurar stackPointer
-		  	 		   move $v1, $v0		# mover $v0 a $v1 (resultado)	 		   
-		  	 		   b restaurarRegistrosDiv	# ir a restaurarRegistros
-		  	 
-		  	 restaurarRegistrosDiv: 
-						lw $t6, 0($sp)	# Restaurar registros utilizados
-						lw $t5, 4($sp)
-						lw $t4, 8($sp)
-						lw $t3, 12($sp)
-						lw $t2, 16($sp)
-						lw $t1, 20($sp)
-						lw $t0, 24($sp)
-						lw $ra, 28($sp)
-		  	 			addi $sp, $sp, 32	# restaurar stackPointer
+		#Restaurar los registros utilizados
+		ldc1 $f6 0($sp)
+		ldc1 $f2 8($sp)
+		lw $t5, 16($sp)
+		lw $t4, 20($sp)
+		lw $t3, 24($sp)
+		lw $t2, 28($sp)
+		lw $t1, 32($sp)
+		lw $t0, 36($sp)
+		lw $ra, 40($sp)
+		addi $sp, $sp, 44	# restaurar stackPointer
 		  	 			
-		  	 			jr $ra			# Salir del procedimiento
-		  	 
-		  	 dividirDecimales:
-		  			bnez $t0, multX100
-		  			jr $ra
-		  			multX100:
-			 			addi $t6, $t6, 100	#sumar 100 al registro donde se va guardando el resultado
-		      	 			addi $t0, $t0, -1	#Sumar 1 al contador 
-						bgtz $t0, multX100	# mientras $t0 > 0 volver a multX100
-		  				move $t0, $t6
-		  				li $t6, 0
-					
-					divDecimales:
-							sub $t0, $t0, $t1		# Dividendo = Dividendo - Divisor
-		      	  				add.s $f2, $f2, $f1		# sumar 1 al resultado (parte decimal)
-			  				bge $t0, $t1, divDecimales	# mientras numero 1 > numero 2 seguir dividiendo
-		  	  				mfc1 $v0, $f2 		# mover $t5 a $v1 (resultado)		  			
-							jr $ra	
-		  	   
-		  	divisionCero:
-		  			l.s $f0, cero
-		  			mfc1 $v0, $f0
-		  			jr $ra
+		jr $ra			# Salir del procedimiento
+		
+	#si dividendo = 0	  	 
+	divisionCero:
+		l.d $f4, ceroDouble
+		jr $ra
 		  			
-		  	divisionInvalida:
-					#Mostrar mensaje 2 al usuario
-			  		li $v0, 4	        #indicar al sistema que se quiere mostrar un string por pantalla
-			  		la $a0, mensaje5    #cargar la dirección de memoria del mensaje 2
-			  		syscall     	 #llamada al sistema: mostrar el mensaje por pantalla
+	#si el divisor es 0 (division por cero)
+	divisionInvalida:
+		#Mostrar mensaje 2 al usuario
+		li $v0, 4	        #indicar al sistema que se quiere mostrar un string por pantalla
+		la $a0, mensaje5    #cargar la dirección de memoria del mensaje 2
+		syscall     	 #llamada al sistema: mostrar el mensaje por pantalla
 			  
-			  		j terminarPrograma
+		j terminarPrograma
+			 
+	multX10:
+		addi $t5, $t5, 10	#sumar 10 al registro donde se va guardando el resultado
+		addi $t0, $t0, -1	#Sumar 1 al contador 
+		bgtz $t0, multX10	# mientras $t0 > 0 volver a multX10
+		move $t0, $t5		#mover $t5 a $t0
+		jr $ra
+			 
+	#Procedimiento para cambiar de signo a un double
+	verificarSignoResultado:
+		beq $t4, 1, cambiar			# si $t4 == 1 (resultado debe ser negativo) cambiar signo
+		jr $ra					# sino salir del procedimiento
+		cambiar:				# cambair signo
+			l.d $f2, ceroDouble
+			sub.d $f4, $f2, $f4	# $f2 = 0 - $f2
+		jr $ra					# salir del procedimiento
 	
 #Procedimiento para cambiar de signo
 #Argumentos:  $a0 = numero
 #Resultado:   $v0
 cambiarSigno:
-		sub $v0, $zero, $a0
-		jr $ra
-
-#Procedimiento para cambiar de signo a uun numero flotante
-#Argumentos:  $a0 = numero
-#Resultado:   $v0
-cambiarSignoFloat:
 		sub $v0, $zero, $a0
 		jr $ra
 
@@ -220,8 +239,8 @@ terminarPrograma:
 		li $v0, 10	    	    #indicarle al sistema que se quiere finalizar el programa	    
 		syscall		    #llamada al sistema: finalizar el programa
 		
-	###### EL RESULTADO DE LA MULTIPLICACION ESTA EN EL REGISTRO $v1 ####################
-	###### EL RESULTADO DE LA MULTIPLICACION ESTA EN EL REGISTRO $v1 ####################
-	###### EL RESULTADO DE LA MULTIPLICACION ESTA EN EL REGISTRO $v1 ####################
-	###### EL RESULTADO DE LA MULTIPLICACION ESTA EN EL REGISTRO $v1 ####################	
+	###### EL RESULTADO DE LA MULTIPLICACION ESTA EN EL REGISTRO $f4 ####################
+	###### EL RESULTADO DE LA MULTIPLICACION ESTA EN EL REGISTRO $f4 ####################
+	###### EL RESULTADO DE LA MULTIPLICACION ESTA EN EL REGISTRO $f4 ####################
+	###### EL RESULTADO DE LA MULTIPLICACION ESTA EN EL REGISTRO $f4 ####################	
 	
